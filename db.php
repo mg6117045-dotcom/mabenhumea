a14fa090288d71a3d6c81f7c765c21c9934931e0
Miguel Angel <mg6117045@gmail.com>
2026-04-29 14:27:50 -0600

    actualizacion

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ function conectarDB() {
     $host = "localhost";
     $db   = "mabenhumea_db";
     $user = "mabenhumea";
-    $pass = "987654320";
+    $pass = "manzana200433?";
     $charset = "utf8mb4";
 
     // El DSN (Data Source Name) define el tipo de driver y los datos del servidor
