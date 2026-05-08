810035202273f018d8ab041c9b9d545de41b2684
Miguel Angel <mg6117045@gmail.com>
2026-05-08 15:43:11 -0600

    mejor

--- login.php
+++ login.php
@@ -33,10 +33,10 @@ require_once 'db.php'; // Traemos el código del otro archivo
             $_SESSION['username'] = $usuario['email']; // Store session data
             $_SESSION['id'] = $usuario['id'];
           
-        $cookie_name  = "id_usuario";
-        $cookie_value = $usuario['id'];   // ← cambiar 'id_usuario' a 'id'
-        $expiry = time() + (86400 * 30);
-        setcookie($cookie_name, $cookie_value, $expiry, "/");
+            $cookie_name = "id_usuario";
+            $cookie_value = $usuario['id_usuario'];
+            $expiry = time() + (86400 * 30); // Valid for 30 days
+            setcookie($cookie_name, $cookie_value, $expiry, "/");
             
             header("Location: dashboard.php");
             
