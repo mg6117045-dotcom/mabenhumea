243e980b23e05bdd30de290bd0946c8d267c975d
Miguel Angel <mg6117045@gmail.com>
2026-05-08 20:32:33 -0600

    listoo

--- agregar_libro.php
+++ agregar_libro.php
@@ -4,13 +4,13 @@ $db = conectarDB();
 
 $datos = [
     'titulo' => $_POST['titulo'],
-    'isbn'   => $_POST['isbn'],
+    'categoria'   => $_POST['categoria'],
     'anio'   => $_POST['anio'],
     'autor'  => $_POST['id_autor'] // Este ID vendría de un <select> de autores
 ];
 
-$sql = "INSERT INTO libros (titulo, isbn, anio_publicacion, id_autor) 
-        VALUES (:titulo, :isbn, :anio, :autor)";
+$sql = "INSERT INTO libros (titulo, categoria, anio_publicacion, id_autor) 
+        VALUES (:titulo, :categoria, :anio, :autor)";
 $db->prepare($sql)->execute($datos);
 header("Location: dashboard.php?msj=LibroAgregado");
 ?>
